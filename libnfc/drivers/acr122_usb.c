2c4781fb0e13f825508c835b30c218d63e133510
Ryan Tenney <rtenney@grubhub.com>
2022-07-22 16:31:07 -0400

    Remove problematic block

--- libnfc/drivers/acr122_usb.c
+++ libnfc/drivers/acr122_usb.c
@@ -434,13 +434,7 @@ acr122_usb_open(const nfc_context *context, const nfc_connstring connstring)
 
       // Check if there are more than 0 alternative interfaces and claim the first one
       if (dev->config->interface->altsetting->bAlternateSetting > 0) {
-        res = usb_set_altinterface(data.pudh, 0);
-        if (res < 0) {
-          log_put(LOG_GROUP, LOG_CATEGORY, NFC_LOG_PRIORITY_ERROR, "Unable to set alternate setting on USB interface (%s)", _usb_strerror(res));
-          usb_close(data.pudh);
-          // we failed to use the specified device
-          goto free_mem;
-        }
+        log_put(LOG_GROUP, LOG_CATEGORY, NFC_LOG_PRIORITY_ERROR, "Skipping USB alternate interface");
       }
 
       // Allocate memory for the device info and specification, fill it and return the info
